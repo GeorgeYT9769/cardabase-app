cfa4ee19430f054cd9a0affd51ea9089dd9a6db5
Juraj Ondovčík <99530555+GeorgeYT9769@users.noreply.github.com>
2025-05-08 17:14:36 +0200

    removed checks for ITF 14 and 16

--- lib/pages/editcard.dart
+++ lib/pages/editcard.dart
@@ -306,16 +306,6 @@ class _EditCardState extends State<EditCard> {
       }
     } else if (cardTypeText == 'CardType.itf') {
       return int.tryParse(eanCode) != null;
-    } else if (cardTypeText == 'CardType.itf14') {
-      return
-        int.tryParse(eanCode) != null
-        && eanCode.length <= 13
-        && eanCode.length >= 14;
-    } else if (cardTypeText == 'CardType.itf16') {
-      return
-        int.tryParse(eanCode) != null
-        && eanCode.length <= 15
-        && eanCode.length >= 16;
     } else if (cardTypeText == 'CardType.upca') {
       if (eanCode.length != 12 || int.tryParse(eanCode) == null) {
         return false;
