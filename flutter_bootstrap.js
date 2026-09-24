87f555d26838fe05e6c2a169a3a7e574e517f0a3
GeorgeYT9769 <jurko.ondovcik@gmail.com>
2026-09-24 15:25:58 +0000

    Deploying to gh-pages from @ GeorgeYT9769/cardabase-app@77ec4082c47561ca8e9ff52761d2eff4e09a5f87 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1058562224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1243704039" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
